7de613a5d314bf9501e96c066f38169a01e73a55
mohit-dhote <mohitdhote465@gmail.com>
2026-04-25 01:00:18 +0530

    added pytest

--- calc.py
+++ calc.py
@@ -1,12 +1,12 @@
 def add(a, b):
-    return a+b
+    return a + b
 
-def sub(a, b):
-    return a-b
+def subtract(a, b):
+    return a - b
 
-def mul(a, b):
-    return a*b
+def multiply(a, b):
+    return a * b
 
-def div(a, b):
-    return a/b
+def divide(a, b):
+    return a / b
 
